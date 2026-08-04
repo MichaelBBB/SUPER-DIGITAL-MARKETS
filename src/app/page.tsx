74620f06c3814342ad24b6f22064ea4010779067
MichaelBBB <54029368+MichaelBBB@users.noreply.github.com>
2026-08-04 09:26:15 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,10 @@
+import PaymentPanel from "@/components/PaymentPanel";
 
+export default function Home() {
+  return (
+    <main className="w-full">
+      {/* This actually loads the payment panel */}
+      <PaymentPanel />
+    </main>
+  );
+}
