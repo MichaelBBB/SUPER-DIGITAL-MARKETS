eda97095b83e591ef33115699bdc962e6289c142
MichaelBBB <54029368+MichaelBBB@users.noreply.github.com>
2026-08-04 13:28:43 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="antialiased bg-[#0f1115]">{children}</body>
+      <body className="bg-[#0f1115] text-white">{children}</body>
     </html>
   );
 }
