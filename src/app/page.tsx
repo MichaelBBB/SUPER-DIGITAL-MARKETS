caf61cdf2a618fd0c565c30c2fb3a76453a38f93
MichaelBBB <54029368+MichaelBBB@users.noreply.github.com>
2026-08-04 15:57:49 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,9 @@
+import PaymentPanel from "@/components/PaymentPanel";
 
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-[#0f1115] text-white">
+      <PaymentPanel />
+    </div>
+  );
+}
