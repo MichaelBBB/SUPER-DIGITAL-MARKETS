c7a3ecbefc2c388aa4eaab4b02b717b32625e3b5
MichaelBBB <54029368+MichaelBBB@users.noreply.github.com>
2026-08-04 15:19:50 +0200

    Delete src/app/page.tsx

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import PaymentPanel from "@/components/PaymentPanel";
-
-export default function Home() {
-  return (
-    <main className="w-full min-h-screen bg-[#0f1115]">
-      <PaymentPanel />
-    </main>
-  );
-}
