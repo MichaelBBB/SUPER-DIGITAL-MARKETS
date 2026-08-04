33404476d2b454896568e585199d3da3d79b676b
MichaelBBB <54029368+MichaelBBB@users.noreply.github.com>
2026-08-04 13:04:39 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import PaymentPanel from "@/components/PaymentPanel";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen">
+    <div className="w-full min-h-screen bg-[#0f1115]">
       <PaymentPanel />
     </div>
   );
