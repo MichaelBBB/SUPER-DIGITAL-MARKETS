2de5a254b50ee26c367d2a465dc584148db6d61b
MichaelBBB <54029368+MichaelBBB@users.noreply.github.com>
2026-08-04 13:42:55 +0200

    Change main background color in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import PaymentPanel from "@/components/PaymentPanel";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full">
+    <main className="w-full min-h-screen bg-[#0f1115]">
       <PaymentPanel />
     </main>
   );
