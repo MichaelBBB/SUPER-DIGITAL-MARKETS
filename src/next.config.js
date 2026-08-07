5c0fdc7d86b0cf06203f01da8b2d5d21d92e7586
MichaelBBB <54029368+MichaelBBB@users.noreply.github.com>
2026-08-07 07:36:38 +0200

    Update next.config.js

--- src/next.config.js
+++ src/next.config.js
@@ -1,8 +1,77 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+2026-08-07T05:08:21.584748474Z ==> Cloning from https://github.com/MichaelBBB/SUPER-DIGITAL-MARKETS
+2026-08-07T05:08:27.096487562Z ==> Checking out commit fe7ff1a2d195fc84c19e37714002acd34fe19545 in branch main
+2026-08-07T05:08:27.801555938Z ==> Using Node.js version 24.14.1 (default)
+2026-08-07T05:08:27.801559941Z ==> Docs on specifying a Node.js version: https://render.com/docs/node-version
+2026-08-07T05:08:27.830929437Z ==> Running build command 'npm install; npm run build'...
+2026-08-07T05:08:40.067228092Z 
+2026-08-07T05:08:40.067242565Z added 531 packages, and audited 532 packages in 12s
+2026-08-07T05:08:40.06728208Z 
+2026-08-07T05:08:40.067385071Z 170 packages are looking for funding
+2026-08-07T05:08:40.067434378Z   run `npm fund` for details
+2026-08-07T05:08:40.152716239Z 
+2026-08-07T05:08:40.152732163Z 12 vulnerabilities (1 low, 1 moderate, 10 high)
+2026-08-07T05:08:40.152733588Z 
+2026-08-07T05:08:40.152735662Z To address issues that do not require attention, run:
+2026-08-07T05:08:40.15273735Z   npm audit fix
+2026-08-07T05:08:40.152738588Z 
+2026-08-07T05:08:40.152740006Z To address all issues, run:
+2026-08-07T05:08:40.152741763Z   npm audit fix --force
+2026-08-07T05:08:40.152742968Z 
+2026-08-07T05:08:40.152744346Z Run `npm audit` for details.
+2026-08-07T05:08:40.321592239Z 
+2026-08-07T05:08:40.321606827Z > super-digital@0.1.0 build
+2026-08-07T05:08:40.321608783Z > next build
+2026-08-07T05:08:40.321610011Z 
+2026-08-07T05:08:40.768390938Z ⚠ No build cache found. Please configure build caching for faster rebuilds. Read more: https://nextjs.org/docs/messages/no-cache
+2026-08-07T05:08:40.836236354Z    ▲ Next.js 15.5.18
+2026-08-07T05:08:40.836280306Z 
+2026-08-07T05:08:40.853414841Z    Creating an optimized production build ...
+2026-08-07T05:08:45.41352196Z  ✓ Compiled successfully in 2.6s
+2026-08-07T05:08:45.414719091Z    Skipping validation of types
+2026-08-07T05:08:45.414744648Z    Skipping linting
+2026-08-07T05:08:45.634548154Z    Collecting page data ...
+2026-08-07T05:08:46.929529182Z    Generating static pages (0/3) ...
+2026-08-07T05:08:47.929364074Z  ✓ Generating static pages (3/3)
+2026-08-07T05:08:48.489815567Z    Finalizing page optimization ...
+2026-08-07T05:08:48.489833214Z    Collecting build traces ...
+2026-08-07T05:08:52.402575448Z 
+2026-08-07T05:08:52.404825855Z Route (app)                                  Size  First Load JS
+2026-08-07T05:08:52.404831747Z ─ ƒ /api/peach/create-checkout                0 B         101 kB
+2026-08-07T05:08:52.404833507Z + First Load JS shared by all              101 kB
+2026-08-07T05:08:52.4048352Z   ├ chunks/4bd1b696-ec67ac596534aad8.js   54.3 kB
+2026-08-07T05:08:52.404836722Z   ├ chunks/58-557f6dc591b37204.js         45.3 kB
+2026-08-07T05:08:52.404838175Z   └ other shared chunks (total)           1.75 kB
+2026-08-07T05:08:52.404839427Z 
+2026-08-07T05:08:52.404840849Z Route (pages)                                Size  First Load JS
+2026-08-07T05:08:52.404842215Z ─ ○ /404                                    226 B        96.5 kB
+2026-08-07T05:08:52.40484356Z + First Load JS shared by all             96.3 kB
+2026-08-07T05:08:52.404844939Z   ├ chunks/framework-e60dca991c46cf12.js  57.5 kB
+2026-08-07T05:08:52.404852391Z   ├ chunks/main-72bc662a1762ef7b.js         37 kB
+2026-08-07T05:08:52.404854158Z   └ other shared chunks (total)           1.75 kB
+2026-08-07T05:08:52.404857878Z 
+2026-08-07T05:08:52.404929183Z ○  (Static)   prerendered as static content
+2026-08-07T05:08:52.404931209Z ƒ  (Dynamic)  server-rendered on demand
+2026-08-07T05:08:52.404932356Z 
+2026-08-07T05:08:53.752013623Z ==> Uploading build...
+2026-08-07T05:09:01.832391608Z ==> Uploaded in 5.7s. Compression took 2.4s
+2026-08-07T05:09:01.836255311Z ==> Build successful 🎉
+2026-08-07T05:09:02.854723392Z ==> Deploying...
+2026-08-07T05:09:02.948464782Z ==> Setting WEB_CONCURRENCY=1 by default, based on available CPUs in the instance
+2026-08-07T05:09:25.484854437Z ==> Running 'npm run start'
+2026-08-07T05:09:26.591036549Z 
+2026-08-07T05:09:26.59106404Z > super-digital@0.1.0 start
+2026-08-07T05:09:26.59107123Z > next start
+2026-08-07T05:09:26.5910741Z 
+2026-08-07T05:09:28.887772843Z    ▲ Next.js 15.5.18
+2026-08-07T05:09:28.887966067Z    - Local:        http://localhost:10000
+2026-08-07T05:09:28.887978877Z    - Network:      http://10.31.37.208:10000
+2026-08-07T05:09:28.888067539Z 
+2026-08-07T05:09:28.888134761Z  ✓ Starting...
+2026-08-07T05:09:30.88021817Z  ✓ Ready in 3.2s
+2026-08-07T05:09:33.703445948Z ==> Your service is live 🎉
+2026-08-07T05:09:33.820361625Z ==> 
+2026-08-07T05:09:33.822520667Z ==> ///////////////////////////////////////////////////////////
+2026-08-07T05:09:33.824893335Z ==> 
+2026-08-07T05:09:33.827161801Z ==> Available at your primary URL https://super-digital-markets-1.onrender.com
+2026-08-07T05:09:33.829860388Z ==> 
+2026-08-07T05:09:33.832651679Z ==> ///////////////////////////////////////////////////////////
