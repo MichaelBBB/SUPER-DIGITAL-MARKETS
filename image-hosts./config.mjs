97c681b0b93cc3845b044974b0e2c2573d148354
MichaelBBB <54029368+MichaelBBB@users.noreply.github.com>
2026-08-09 15:46:52 +0200

    Update config.mjs

--- image-hosts./config.mjs
+++ image-hosts./config.mjs
@@ -1,1 +1,15 @@
-
+export const imageHosts = [
+  {
+    hostname: "localhost",
+    protocol: "http",
+  },
+  {
+    hostname: "assets.vercel.com",
+  },
+  {
+    hostname: "images.unsplash.com",
+  },
+  {
+    hostname: "via.placeholder.com",
+  },
+];
