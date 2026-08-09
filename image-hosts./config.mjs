c6453e4afc460e5f8d5b9d82a2a42aeef34eb724
MichaelBBB <54029368+MichaelBBB@users.noreply.github.com>
2026-08-09 21:12:14 +0200

    Refactor imageHosts array initialization

--- image-hosts./config.mjs
+++ image-hosts./config.mjs
@@ -1,15 +1,6 @@
 export const imageHosts = [
-  {
-    hostname: "localhost",
-    protocol: "http",
-  },
-  {
-    hostname: "assets.vercel.com",
-  },
-  {
-    hostname: "images.unsplash.com",
-  },
-  {
-    hostname: "via.placeholder.com",
-  },
+  { hostname: "localhost", protocol: "http" },
+  { hostname: "assets.vercel.com" },
+  { hostname: "images.unsplash.com" },
+  { hostname: "via.placeholder.com" },
 ];
