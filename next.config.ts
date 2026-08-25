e8b5aee617ed6f159ddd9af6e51091f084588d9d
MichaelBBB <54029368+MichaelBBB@users.noreply.github.com>
2026-08-25 22:29:21 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,10 @@
+// next.config.ts
+import type { NextConfig } from "next";
 
+const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_PEACH_ENTITY_ID: process.env.NEXT_PUBLIC_PEACH_ENTITY_ID || '',
+  },
+};
+
+export default nextConfig;
