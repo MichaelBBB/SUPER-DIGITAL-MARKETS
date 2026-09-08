53ab09e328014ef47f7570e1a61c432654401f2e
MichaelBBB <54029368+MichaelBBB@users.noreply.github.com>
2026-09-08 16:48:49 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.peachpayments.com https://pnc43975.jscrambler.com https://cdn.peachpayments.com; style-src 'self' 'unsafe-inline' https://*.peachpayments.com https://fonts.googleapis.com; frame-src 'self' https://*.peachpayments.com https://secure.peachpayments.com; connect-src 'self' https://*.peachpayments.com wss://*.peachpayments.com; img-src 'self' data: https://*.peachpayments.com https://fonts.gstatic.com; font-src 'self' data: https://fonts.gstatic.com;",
+          },
+        ],
+      },
+    ];
+  },
+};
 
+module.exports = nextConfig;
